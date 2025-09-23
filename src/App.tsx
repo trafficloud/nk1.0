1b994631f54bc9a66be4cb89b1d66bfb2b2b93ee
trafficloud <romanovskiy.s@icloud.com>
2025-09-23 17:01:24 +0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,32 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Benefits from './components/Benefits';
 import Calculator from './components/Calculator';
 import Portfolio from './components/Portfolio';
+import FullPortfolioPage from './pages/FullPortfolioPage';
 
 function App() {
   return (
-    <div className="min-h-screen bg-white font-space">
-      <Header />
-      <Hero />
-      <Benefits />
-      <Calculator />
-      <Portfolio />
+    <div className="min-h-screen bg-white">
+      <Routes>
+        <Route path="/" element={
+          <>
+            <Header />
+            <Hero />
+            <Benefits />
+            <Portfolio />
+            <Calculator />
+          </>
+        } />
+        <Route path="/portfolio" element={
+          <>
+            <Header />
+            <FullPortfolioPage />
+          </>
+        } />
+      </Routes>
     </div>
   );
 }
