a350745b7c3d841ee18649da72cdc5f16a4cad0a
trafficloud <romanovskiy.s@icloud.com>
2025-09-23 17:00:28 +0300

    Исправить отсутствующие импорты в App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
 import Hero from './components/Hero';
 import Benefits from './components/Benefits';
 import Calculator from './components/Calculator';
 import Portfolio from './components/Portfolio';
+import FullPortfolioPage from './pages/FullPortfolioPage';
 
 function App() {
   return (
