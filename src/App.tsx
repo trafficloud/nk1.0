8363f02c09903ab81bd202f2a3e4a9f475817c4c
trafficloud <romanovskiy.s@icloud.com>
2025-09-23 16:58:07 +0300

    Updated package-lock.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Benefits from './components/Benefits';
 import Calculator from './components/Calculator';
 import Portfolio from './components/Portfolio';
-import FullPortfolioPage from './pages/FullPortfolioPage';
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-white font-space">
-        <Routes>
-          <Route path="/" element={
-            <>
-              <Header />
-              <Hero />
-              <Benefits />
-              <Calculator />
-              <Portfolio />
-            </>
-          } />
-          <Route path="/portfolio" element={
-            <>
-              <Header />
-              <FullPortfolioPage />
-            </>
-          } />
-        </Routes>
-      </div>
-    </Router>
+    <div className="min-h-screen bg-white font-space">
+      <Header />
+      <Hero />
+      <Benefits />
+      <Calculator />
+      <Portfolio />
+    </div>
   );
 }
 
