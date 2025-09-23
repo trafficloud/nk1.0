82bc917481ce1e87caea8bede71e64ff025515a8
trafficloud <romanovskiy.s@icloud.com>
2025-09-23 16:59:28 +0300

    Исправить ошибку контекста Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from './components/Header';
+import { Routes, Route } from 'react-router-dom';
 import Hero from './components/Hero';
 import Benefits from './components/Benefits';
 import Calculator from './components/Calculator';
@@ -7,14 +7,22 @@ import Portfolio from './components/Portfolio';
 
 function App() {
   return (
-    <div className="min-h-screen bg-white font-space">
-      <Header />
-      <Hero />
-      <Benefits />
-      <Calculator />
-      <Portfolio />
+    <div className="min-h-screen bg-white">
+      <Routes>
+        <Route path="/" element={
+          <>
+            <Header />
+            <Hero />
+            <Benefits />
+            <Portfolio />
+            <Calculator />
+          </>
+        } />
+        <Route path="/portfolio" element={
+          <>
+            <Header />
+            <FullPortfolioPage />
+          </>
+        } />
+      </Routes>
     </div>
-  );
-}
-
-export default App;
